c9d87304f92b476e6d2c72f21cac30af21f8e275
adhithan-m <116782079+adhithan-m@users.noreply.github.com>
2022-12-26 16:19:14 +0530

    loop-1.

---
+++ for/for.js
@@ -0,0 +1,58 @@
+//for ; 
+ 
+//for (let a=0; a<=10; a++)
+//{
+ //console.log("number"+" "+a);
+//}
+
+//for (let a=1; a<=20; a++)
+//{
+	//if(a%2==0)
+	//{
+	//console.log("number"+" "+a);
+    //}
+//}	
+
+//for (let a=20; a>=0; a--)
+//{
+	//if(a%2==0)
+	//{
+	//console.log("number"+" "+a);
+    //}
+//}
+
+//var num=parseInt(prompt("enter number :"));
+//n=0
+//for(a=num;num%2==0&&a==num;a++)
+//{
+	//document.write(num+" is even");
+	//n+=1;
+//}
+//for(n1=n;n1==0;n1++)
+//{
+	//document.write(num+" is odd");
+//}
+
+//var num=parseInt(prompt("enter number :"));
+//n=0
+//for(a=num;num>2==0&&a==num;a++)
+//{
+    //alert(num+" is positive");
+	//n+=1;
+//}
+//for(n1=n;n1==0;n1++)
+//{
+	//alert(num+" is negative");
+//}
+
+
+var i = parseInt(prompt("Enter the number"));
+
+if (i>=0)
+{
+	alert("Enter the number is positive");
+}
+else
+{
+	alert("Enter the number is negative");
+}
